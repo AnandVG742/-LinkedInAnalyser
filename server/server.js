213476aced499555375d280d20d1c192510adef3
anandha  narayanan <anandhanarayanan742@gmail.com>
2025-03-19 09:38:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const analyzeRoute = require('./routes/analyseRoute');
+const analyzeRoute = require('./routes/analyseRoutes');
 require('dotenv').config();
 
 const app = express();
